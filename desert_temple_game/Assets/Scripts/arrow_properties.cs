11f87ccfdaf5500339bccdbadb3681764a8a0e72
daniadib <56983621+daniadib@users.noreply.github.com>
2021-08-17 19:31:51 -0400

    Unfinished Level 1
    - To see inside, move one of the walls & the ceiling out of the way
    - Main path built
    - No modeling yet, plain materials
    - Working arrow trap
    - Incomplete clashing walls trap
    - To see arrow trap, go to arrow_trap_trigger in hierarchy and check the box saying check_for_arrow_trap

---
+++ desert_temple_game/Assets/Scripts/arrow_properties.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrow_properties : MonoBehaviour
+{
+    Vector3 velocity = new Vector3(0,2f,0);
+    // Start is called before the first frame update
+    void Start()
+    {
+      Destroy(gameObject, 0.05f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(velocity);
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+      Destroy(gameObject);
+    }
+}
